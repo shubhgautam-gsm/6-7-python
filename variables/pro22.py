f37a73eed46168c090690a36196e59eba33289af
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-09-15 19:08:17 +0530

    15-9-25

--- variables/pro22.py
+++ variables/pro22.py
@@ -2,9 +2,8 @@ n='$'
 
 for i in range(1,6):
     print(i)
-    
-    
-    
+
+
 
 # 'bhumi 2 appt'
 #   'devam',
--- variables/pro23.py
+++ variables/pro23.py
@@ -2,14 +2,17 @@ n='$'
 
 for i in range(1,5):
     
-    if(i==4):
-     for j in range(1,364):     
+    if(i==4):#(aa)
+     for j in range(1,6):     
         print(j,end='')
-    else:
-        for j in range(1,365):     
+    else:#(athva aa)
+        for j in range(1,9):     
          print(j,end='')
     
     print()
 
-
+# 12345678
+# 12345678
+# 12345678
+# 12345
 # row =3 column=8
